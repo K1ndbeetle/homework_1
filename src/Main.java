8f503eb79b9454588382bf45035d272b70e3859c
K1ndbeetle <alx-s@bk.ru>
2022-11-12 11:52:52 +0300

    Homework2

--- src/Main.java
+++ src/Main.java
@@ -27,5 +27,26 @@ public class Main {
         System.out.println("dog = " + dog);
         System.out.println("cat = " + cat);
         System.out.println("paper = " + paper);
+
+        //homework2
+        //task №4
+        System.out.println("Задача №4");
+        var friend = 19;
+        System.out.println("friend = " + friend);
+        friend += 2;
+        System.out.println("friend = " + friend);
+        friend /= 7;
+        System.out.println("friend = " + friend);
+
+        //task №5
+        System.out.println("Задача №5");
+        var frog = 3.5;
+        System.out.println("frog = " + frog);
+        frog *= 10;
+        System.out.println("frog = " + frog);
+        frog /= 3.5;
+        System.out.println("frog = " + frog);
+        frog += 4;
+        System.out.println("frog = " + frog);
     }
 }
